af99f1ae5b0a266fdee3b75c5986f305de96f9e0
Petro Sidlovskyy <sidlovskyy@gmail.com>
2018-04-25 01:37:01 +0300

    Fix merge issues

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -8,4 +8,12 @@ export const environment = {
   HttpProvider: "http://52.51.85.249:8545",
   // HttpProvider: "http://127.0.0.1:8545"
   cookiesDomain: "localhost",
+  contracts: {
+    swap: {
+      address: {
+        AeroToErc20: "0x597c59b85b9d75abb040091e084ffdbe687ac2f0",
+        Erc20ToErc20: "0xb08d606c34e2b259512be2d06e9748af662ede71"
+      }
+    }
+  }
 };
