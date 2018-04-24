4be337d81b76d369a79373be51a44323c63e9b5a
paddy <paddy@localhost.localdomain>
2018-04-24 17:16:20 +0200

    fix broken header

--- src/app/shared/components/sidebar-header/sidebar-header.component.ts
+++ src/app/shared/components/sidebar-header/sidebar-header.component.ts
@@ -25,7 +25,6 @@ export class SidebarHeaderComponent implements OnInit {
     //     this.address = this.sessionStorageService.retrieve('acc_address');
     //     this.avatar = avatars.create(this.address);
     //   });
-    }
   }
 
 }
