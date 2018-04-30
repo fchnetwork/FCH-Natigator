f62fe13da1cf48d13cc3eb30f0152f588b323009
Radoslaw Fabisiak <fabisiakradoslaw@gmail.com>
2018-04-30 12:50:18 +0200

    Implement returnUrl in unlock screen.

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication-service/authentication.service';
 import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Subject } from 'rxjs/Subject';
 import { SessionStorageService } from 'ngx-webstorage';
@@ -21,8 +22,9 @@ export class LoginComponent implements OnInit {
   avatar: string;
   private: string; // i dont know 
   loginForm: FormGroup;
-
-  step = 'step_1';  // default page to show
+  sub: any;
+  step = 'step_1';
+  redirectUrl: string;
 
   windowState: boolean;
 
@@ -31,6 +33,7 @@ export class LoginComponent implements OnInit {
     private router: Router,
     public formBuilder: FormBuilder,
     public sessionStorageService: SessionStorageService,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
@@ -42,15 +45,32 @@ export class LoginComponent implements OnInit {
     }, {
         // validator: this.matchingPasswords('password', 'confirmpassword')
       });
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        // this.page = +params['page'] || 0;
+        this.redirectUrl = params.redirectUrl || 'dashboard';
+      });
   }
 
   onSubmitAddress() {
-    this.authServ.login(this.password);
+    this.authServ.login(this.password).then((res)=>{
+      console.log(res);
+      if(res === 'success') {
+        console.log(this.redirectUrl);
+        this.router.navigate([`/${this.redirectUrl}`]);
+      }
+    });
   }
 
   windowStateChange() {
     if (this.windowState) {
       this.router.navigate(['/account/recovery']);
     }
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
