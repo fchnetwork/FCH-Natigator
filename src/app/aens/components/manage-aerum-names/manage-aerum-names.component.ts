7d8f7a092af35bba809299379cdf0f8b0f8d4627
Petro Sidlovsky <sidlovskyy@gmail.com>
2018-05-09 13:19:31 +0300

    ANS: Add basic ANS admin functionality

--- src/app/aens/components/manage-aerum-names/manage-aerum-names.component.ts
+++ src/app/aens/components/manage-aerum-names/manage-aerum-names.component.ts
@@ -5,6 +5,8 @@ import { TranslateService } from '@ngx-translate/core';
 
 import { ModalService } from '@app/shared/services/modal.service';
 import { AuthenticationService } from '@app/account/services/authentication-service/authentication.service';
+import { Address } from 'cluster';
+import { timeHour } from 'd3-time';
 
 @Component({
   selector: 'app-manage-aerum-names',
@@ -21,6 +23,10 @@ export class ManageAerumNamesComponent implements OnInit {
   nameAvailable: boolean;
   processing: boolean;
 
+  isOwner: boolean;
+  newPrice: number;
+  transferTo: string;
+
   constructor(
     private authService: AuthenticationService,
     private modalService: ModalService,
@@ -32,18 +38,25 @@ export class ManageAerumNamesComponent implements OnInit {
   ngOnInit() {
     const keystore = this.authService.getKeystore();
     this.account  = "0x" + keystore.address;
+    this.transferTo = this.account;
 
     // TODO: Test code. Remove later
     this.name = 'asrcrypto';
     this.price = 0.01;
+    this.newPrice = this.price;
+    this.isOwner = true;
   }
 
   async checkName() {
     try {
+      this.startProcessing();
       await this.tryCheckName();
     } catch (e) {
       this.notificationService.notify(this.translate('ENS.UNHANDLED_ERROR'), `${this.translate('ENS.CHECK_NAME_ERROR')}: ${this.name}.aer`, 'aerum', 5000);
       throw e;
+    }
+    finally{
+      this.stopProcessing();
     }
   }
 
@@ -55,11 +68,15 @@ export class ManageAerumNamesComponent implements OnInit {
 
   async buyName() {
     try {
+      this.startProcessing();
       await this.tryBuyName();
       this.notificationService.notify(this.translate('ENS.NAME_BUY_SUCCESS_TITLE'), `${this.translate('ENS.NAME_BUY_SUCCESS')}: ${this.name}.aer`, 'aerum');
     } catch (e) {
       this.notificationService.notify(this.translate('ENS.UNHANDLED_ERROR'), `${this.translate('ENS.BUY_NAME_ERROR')}: ${this.name}.aer`, 'aerum', 5000);
       throw e;
+    }
+    finally{
+      this.stopProcessing();
     }
   }
 
@@ -73,8 +90,49 @@ export class ManageAerumNamesComponent implements OnInit {
     // TODO: buy
   }
 
+  async setPrice() {
+    try {
+      this.startProcessing();
+      await this.trySetPrice();
+    } catch (e) {
+      this.notificationService.notify(this.translate('ENS.UNHANDLED_ERROR'), `${this.translate('ENS.SET_PRICE_ERROR')}: ${this.name}.aer`, 'aerum', 5000);
+      throw e;
+    }
+    finally{
+      this.stopProcessing();
+    }
+  }
+
+  async trySetPrice() {
+
+  }
+
+  async setOwner() {
+    try {
+      this.startProcessing();
+      await this.trySetOwner();
+    } catch (e) {
+      this.notificationService.notify(this.translate('ENS.UNHANDLED_ERROR'), `${this.translate('ENS.SET_OWNER_ERROR')}: ${this.name}.aer`, 'aerum', 5000);
+      throw e;
+    }
+    finally{
+      this.stopProcessing();
+    }
+  }
+
+  async trySetOwner() {
+
+  }
+
+  private startProcessing() {
+    this.processing = true;
+  }
+
+  private stopProcessing() {
+    this.processing = false;
+  }
+
   private translate(key: string) {
     return this.translateService.instant(key);
   }
-
 }
