2dded57fb948f2f20633c69face6213bddbe5cbe
bballay <bballay@asrcrypto.io>
2018-05-14 16:06:56 +0200

    Resolving bugs with the active group expand functionality

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,26 +1,70 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { Router, RoutesRecognized, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { AuthenticationService } from '@app/core/authentication-service/authentication.service';
-import { promise } from 'protractor';
+import { Component, OnInit, ViewChild, AfterViewInit, EventEmitter, AfterViewChecked, ChangeDetectorRef, OnDestroy, AfterContentInit, AfterContentChecked } from '@angular/core';
+import { Router, RoutesRecognized, ActivatedRoute, NavigationEnd, Data } from '@angular/router';
+import { AuthenticationService } from '@app/core/authentication-service/authentication.service'; 
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-wallet',
   templateUrl: './wallet.component.html',
   styleUrls: ['./wallet.component.scss']
 })
-export class WalletComponent { 
+export class WalletComponent implements AfterViewChecked, OnDestroy {
+  @ViewChild('sidebar') sidebar: any;
+  viewLoaded$ = new EventEmitter<any>();
+  sidebarLoaded$: EventEmitter<any>;
+  routeData$: Subscription;
 
   constructor(
     private authService: AuthenticationService,
     public router: Router,
-    public activeRoute: ActivatedRoute
-  ) { }
+    public activeRoute: ActivatedRoute,
+    private changeDetector: ChangeDetectorRef
+  ) {
+    this.routeData$ = this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .map(() => this.activeRoute)
+      .map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      })
+      .mergeMap(route => route.data)
+      .subscribe(currentData => {
+        if (currentData && currentData.sidebarGroup) {
+          console.log('Selected the ' + currentData.sidebarGroup + ' group');
+
+          this.viewLoaded$.subscribe(w => {
+            console.log('view loaded');
+            this.sidebar.toggleGroup(currentData.sidebarGroup); 
+            this.viewLoaded$.complete();
+          });
+        }
+      });
+  }
 
   logout() {
     this.authService.logout();
   }
 
   home() {
-    this.router.navigate(['/dashboard']);
-  }  
+    this.router.navigate(['/wallet/home']);
+  }
+
+  ngAfterViewChecked() {
+    // Preventing Angular ExpressionChangedAfterItHasBeenCheckedError.
+    this.changeDetector.detectChanges();
+
+    if (this.sidebarLoaded$ == null) {
+      this.sidebarLoaded$ = this.sidebar.sidebarLoaded.subscribe(w => { 
+        this.viewLoaded$.emit();
+        this.sidebarLoaded$.complete();
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.viewLoaded$.unsubscribe();
+    this.sidebarLoaded$.unsubscribe();
+    this.routeData$.unsubscribe();
+  }
 }
