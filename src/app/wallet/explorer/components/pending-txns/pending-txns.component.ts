b4f06c765bc411db393636a6985318a64ba8c729
Petro Sidlovsky <sidlovskyy@gmail.com>
2018-05-15 17:31:57 +0300

    Remove karma, jasmine & protractor

--- src/app/wallet/explorer/components/pending-txns/pending-txns.component.ts
+++ src/app/wallet/explorer/components/pending-txns/pending-txns.component.ts
@@ -1,4 +1,3 @@
-import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';  
