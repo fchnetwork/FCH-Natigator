8dc7101cf367afacfeb78ed054e5ff6b1417fc84
Petro Sidlovsky <sidlovskyy@gmail.com>
2018-05-16 09:45:20 +0300

    Unit tests. Fixing issues with webpack aliases

--- config/webpack.aliases.js
+++ config/webpack.aliases.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    '@app': path.resolve('src/app/app'),
+    '@app': path.resolve('src/app'),
+    '@core': path.resolve('src/app/core'),
     '@account': path.resolve('src/app/account'),
+    '@shared': path.resolve('src/app/shared'),
     '@dashboard': path.resolve('src/app/wallet/dashboard'),
     '@explorer': path.resolve('src/app/wallet/explorer'),
-    '@shared': path.resolve('src/app/shared'),
     '@transaction': path.resolve('src/app/wallet/transaction'),
     '@diagnostics': path.resolve('src/app/wallet/diagnostics'),
     '@env': path.resolve('src/environments')
