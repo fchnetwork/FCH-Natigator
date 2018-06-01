c93bb73926603c0f2ce7fdfb2708a1bea345432c
Petro Sidlovsky <sidlovskyy@gmail.com>
2018-06-01 15:56:38 +0300

    Add aerum erc20 swap service

--- src/app/core/swap/cross-chain/aerum-erc20-swap-service/aerum-erc20-swap.service.ts
+++ src/app/core/swap/cross-chain/aerum-erc20-swap-service/aerum-erc20-swap.service.ts
@@ -16,4 +16,39 @@ export class AerumErc20SwapService extends BaseContractService {
     super(artifacts.abi, environment.contracts.swap.crossChain.address.aerum.Erc20Swap, authenticationService, contractExecutorService);
   }
 
+  async openSwap(hash: string, erc20Value: string, erc20ContractAddress: string, withdrawTrader: string, timelock: number) {
+    const openSwap = this.contract.methods.open(
+      hash,
+      erc20Value,
+      erc20ContractAddress,
+      withdrawTrader,
+      timelock.toString(10)
+    );
+    const receipt = await this.contractExecutorService.send(openSwap);
+    return receipt;
+  }
+
+  async closeSwap(hash: string, secretKey: string) {
+    const closeSwap = this.contract.methods.close(hash, this.web3.utils.fromAscii(secretKey));
+    const receipt = await this.contractExecutorService.send(closeSwap);
+    return receipt;
+  }
+
+  async expireSwap(hash: string) {
+    const expireSwap = this.contract.methods.expire(hash);
+    const response = await this.contractExecutorService.send(expireSwap);
+    return response;
+  }
+
+  async checkSwap(hash: string) {
+    const checkSwap = this.contract.methods.check(hash);
+    const receipt = await this.contractExecutorService.call(checkSwap);
+    return receipt;
+  }
+
+  async checkSecretKey(hash: string) {
+    const checkSecretKey = this.contract.methods.checkSecretKey(hash);
+    const receipt = await this.contractExecutorService.call(checkSecretKey);
+    return receipt;
+  }
 }
--- src/app/external/ethereum-wallet/ethereum-wallet.component.ts
+++ src/app/external/ethereum-wallet/ethereum-wallet.component.ts
@@ -6,6 +6,7 @@ import { Guid } from "@shared/helpers/guid";
 import { AeroSwapService } from "@core/swap/cross-chain/aero-swap-service/aero-swap.service";
 import { AuthenticationService } from "@core/authentication/authentication-service/authentication.service";
 import { LoggerService } from "@core/general/logger-service/logger.service";
+import { AerumErc20SwapService } from "@core/swap/cross-chain/aerum-erc20-swap-service/aerum-erc20-swap.service";
 
 @Component({
   selector: 'app-ethereum-wallet',
@@ -19,7 +20,8 @@ export class EthereumWalletComponent implements OnInit {
   constructor(
     private logger: LoggerService,
     private authenticationService: AuthenticationService,
-    private aeroSwapService: AeroSwapService
+    private aeroSwapService: AeroSwapService,
+    private aerumErc20SwapService: AerumErc20SwapService
   ) { }
 
   ngOnInit() {
@@ -28,6 +30,11 @@ export class EthereumWalletComponent implements OnInit {
   }
 
   async accept() {
+    await this.testAeroSwap();
+    await this.testAerumErc20Swap();
+  }
+
+  async testAeroSwap() {
     const secret = Guid.newGuid();
     const hash = sha3(secret);
     const aero = '0.01';
@@ -36,6 +43,21 @@ export class EthereumWalletComponent implements OnInit {
 
     this.logger.logMessage(`Secret: ${secret}, hash: ${hash}, timestamp: ${timestamp}, trader: ${this.address}`);
     await this.aeroSwapService.openSwap(hash, aero, this.address, timestamp);
+    this.logger.logMessage(`Swap created: ${hash}`);
+    const swap = await this.aeroSwapService.checkSwap(hash);
+    this.logger.logMessage(`Swap checked: ${hash}`, swap);
+  }
+
+  async testAerumErc20Swap() {
+    const secret = Guid.newGuid();
+    const hash = sha3(secret);
+    const amount = (10 * Math.pow(10,18)).toString(10);
+    const tokenAddress = '';
+    const timeout = 120;
+    const timestamp = Math.ceil((new Date().getTime() / 1000) + timeout);
+
+    this.logger.logMessage(`Secret: ${secret}, hash: ${hash}, timestamp: ${timestamp}, trader: ${this.address}, token: ${tokenAddress}`);
+    await this.aerumErc20SwapService.openSwap(hash, amount, tokenAddress, this.address, timestamp);
     this.logger.logMessage(`Swap created: ${hash}`);
     const swap = await this.aeroSwapService.checkSwap(hash);
     this.logger.logMessage(`Swap checked: ${hash}`, swap);
