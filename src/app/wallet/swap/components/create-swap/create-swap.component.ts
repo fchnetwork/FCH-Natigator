b704378858da49ac56c9f22d45c3cd6594af0e23
Radoslaw Fabisiak <fabisiakradoslaw@gmail.com>
2018-06-08 15:24:09 +0200

    Resolve ans name for address in swap counterparty fix

--- src/app/wallet/swap/components/create-swap/create-swap.component.ts
+++ src/app/wallet/swap/components/create-swap/create-swap.component.ts
@@ -142,7 +142,7 @@ export class CreateSwapComponent implements OnInit {
 
   private async confirmAndCreateSwap() {
     this.counterpartyAddress = this.counterpartyAddress.replace(/\s+/g, '');
-    this.counterpartyAddress = await this.aensService.resolveAddressFromName(this.counterpartyAddress);
+    this.counterpartyAddress = await this.aensService.safeResolveNameOrAddress(this.counterpartyAddress);
     const modalResult = await this.modalService.openSwapCreateConfirm({
       swapId: this.swapId,
       token: this.token,
@@ -181,7 +181,7 @@ export class CreateSwapComponent implements OnInit {
       this.swapId,
       this.tokenAmount.toString(10),
       counterpartyTokenAmount.toString(10),
-      await this.aensService.resolveNameOrAddress(this.counterpartyAddress),
+      await this.aensService.safeResolveNameOrAddress(this.counterpartyAddress),
       this.counterpartyToken.address
     );
   }
@@ -194,7 +194,7 @@ export class CreateSwapComponent implements OnInit {
       tokenAmount.toString(10),
       this.token.address,
       this.counterpartyTokenAmount.toString(10),
-      await this.aensService.resolveNameOrAddress(this.counterpartyAddress),
+      await this.aensService.safeResolveNameOrAddress(this.counterpartyAddress),
     );
   }
 
@@ -207,7 +207,7 @@ export class CreateSwapComponent implements OnInit {
       tokenAmount.toString(10),
       this.token.address,
       counterpartyTokenAmount.toString(10),
-      await this.aensService.resolveNameOrAddress(this.counterpartyAddress),
+      await this.aensService.safeResolveNameOrAddress(this.counterpartyAddress),
       this.counterpartyToken.address
     );
   }
