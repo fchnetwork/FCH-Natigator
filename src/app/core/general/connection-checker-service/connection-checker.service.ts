f467b7160ac82e7aed6413d1bfd3d022d29dadaf
jonasboardwalker <d.baznamin@asrcrypto.io>
2018-06-08 16:40:02 +0200

    Connection checker fixed

--- src/app/core/general/connection-checker-service/connection-checker.service.ts
+++ src/app/core/general/connection-checker-service/connection-checker.service.ts
@@ -2,74 +2,99 @@ import { Injectable } from '@angular/core';
 import { NotificationMessagesService } from '@core/general/notification-messages-service/notification-messages.service';
 import { LoaderService } from '@core/general/loader-service/loader.service';
 import { environment } from '@env/environment';
+import { LoggerService } from '@app/core/general/logger-service/logger.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class ConnectionCheckerService {
 
-    connected: boolean = false;
+    private connected: boolean = false;
+    private heartbeat_interval: any = null; 
+    private missed_heartbeats: number = 0;
+    private socket_id: number = 0;
+    private heartbeat_msg: string = '{"jsonrpc":"2.0","method":"net_listening","params":[],"id":67}';
 
-    private readonly STATE_OPEN: number = 1;
-    private readonly STATE_CLOSED: number = 3;
-
-    private websocket: WebSocket = new WebSocket(environment.WebsocketProvider);
-    private pingInterval: number = 10000;
-    private onDeath: () => any;
-    private alive: boolean = true;
+    private websocket: WebSocket;
 
     constructor(
         private notificationMessagesService: NotificationMessagesService,
         private loaderService: LoaderService,
+        private logger: LoggerService,
+        private translateService: TranslateService
     ) {
-        this.initWebsocket();
-        // this.loaderService.toggle(true, "Connecting to aerum network...");
+        this.initWebsocket(); 
+    }
+
+    protected translate(key: string): string {
+        return this.translateService.instant(key);
     }
 
     private initWebsocket() {
-        this.websocket = new WebSocket(environment.WebsocketProvider);
-        // this.loaderService.toggle(true, "Connecting to aerum network...");
-
-        this.websocket.addEventListener('open', res => {
-            if (this.connected) {
-                this.connected = true;
-                this.notificationMessagesService.connectionConnected();
-            }
-        });
-
-        this.websocket.addEventListener('close', res => {
-            if (this.connected) {
-                this.connected = false;
-                this.notificationMessagesService.connectionDisconnected(); 
-                this.initWebsocket();
-            }
-        });
-
-        this.websocket.addEventListener('error', res => {
-            if (this.connected) {
-                this.connected = false;
-                this.notificationMessagesService.connectionDisconnected();
-                this.initWebsocket();
-            }
-        });
+        this.websocket = new WebSocket(environment.WebsocketProvider); 
+        this.socket_id = Math.random() * 100; 
+        this.websocket.addEventListener('open', ev => this.onOpen(ev));
+        this.websocket.addEventListener('message', ev => this.onMessage(ev));
+        this.websocket.addEventListener('error', ev => this.onError(ev));  
     }
 
-   
+    private onOpen(ev: Event) {
+        // add closed handler after ws is initialized
+        this.websocket.addEventListener('close', ev => this.onClosed(ev));
+ 
+        this.loaderService.toggle(false);
+        this.notificationMessagesService.connectionConnected();
+        this.connected = true; 
 
-    // private websocketCheck() {
-    //     let websocket = new WebSocket(environment.WebsocketProvider);
+        if (this.heartbeat_interval === null) {
+            this.missed_heartbeats = 0;
+            this.heartbeat_interval = setInterval(this.checkHeartbeat, 3000, this.missed_heartbeats, this.websocket, this.heartbeat_interval, this.heartbeat_msg, this.logger);
+        }
+    }
 
-    //     websocket.addEventListener('open', res => {
-    //        if (!this.connected) {
-    //             this.connected = true;
-    //             this.notificationMessagesService.connectionConnected();
-    //        }
-    //     });
+    private onClosed(ev: CloseEvent) {
+        this.loaderService.toggle(true, this.translateService.instant("CONNECTION.CONNECTING_TO_AERUM"));
 
-    //     websocket.addEventListener('close', res => {
-    //         if (this.connected) {
-    //             this.connected = false;
-    //             this.notificationMessagesService.connectionDisconnected();
-    //         }
-    //     });
-    // }
+        clearInterval(this.heartbeat_interval);
+ 
+        if (this.connected) {
+            this.notificationMessagesService.connectionDisconnected();
+            this.connected = false;
+        }
 
+        this.initWebsocket(); 
+    }
+
+    private onError(ev: Event) { 
+        clearInterval(this.heartbeat_interval);
+
+        if(this.websocket != null && this.websocket.readyState == this.websocket.CLOSED) {
+            this.loaderService.toggle(true, this.translateService.instant("CONNECTION.CONNECTING_TO_AERUM"));
+        }
+
+        if(!this.connected) { 
+            this.initWebsocket();
+        } 
+    }
+
+    private onMessage(ev: MessageEvent) {
+        this.logger.logMessage('Message from server ', ev.data);
+    }
+
+    private checkHeartbeat(missed_heartbeats, websocket, heartbeat_interval, heartbeat_msg, logger) {
+        try {
+            missed_heartbeats += 1;
+
+            if (missed_heartbeats >= 3) {
+                throw new Error("Too many missed heartbeats.");
+            }
+
+            websocket.send(heartbeat_msg);
+            logger.logMessage(websocket);
+        } catch (e) {
+            clearInterval(heartbeat_interval);
+            heartbeat_interval = null;
+            logger.logWarning("Closing connection. Reason: " + e.message);
+            websocket.close();
+        }
+    } 
 }
