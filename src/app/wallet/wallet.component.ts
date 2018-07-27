216cb1177b8761e02bcc286f231cf4b2be9c5e02
Volodymyr Matskiv <v.matskiv@asrcrypto.io>
2018-07-27 14:34:10 +0300

    Fixed autoclose for all items

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -42,11 +42,10 @@ export class WalletComponent implements AfterViewChecked, OnDestroy {
       })
       .mergeMap(route => route.data)
       .subscribe(currentData => {
-        if (currentData && currentData.sidebarGroup) {
-          if( this.sidebar.isToggled) {
-            this.sidebar.toggleSidebar();
-          }
-
+        if(this.sidebar.isToggled) {
+          this.sidebar.toggleSidebar();
+        }
+        if (currentData && currentData.sidebarGroup) { 
           this.viewLoaded$.subscribe(w => {
             this.sidebar.toggleGroup(currentData.sidebarGroup);
             this.viewLoaded$.complete();
