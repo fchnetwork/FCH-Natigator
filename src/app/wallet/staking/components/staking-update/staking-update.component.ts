1f7835bb85bf789ead580a3bf4541b9a5a141bc9
Volodymyr Matskiv <v.matskiv@asrcrypto.io>
2018-08-02 16:15:10 +0300

    Added staking update UI parts of component

--- src/app/wallet/staking/components/staking-update/staking-update.component.ts
+++ src/app/wallet/staking/components/staking-update/staking-update.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-staking-update',
   templateUrl: './staking-update.component.html',
-  styleUrls: ['./staking-update.component.scss']
+  styleUrls: ['./staking-update.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class StakingUpdateComponent implements OnInit {
+
+  increaseAmount: number;
+  decreaseAmount: number;
+
+  delegeteAddress = '0xe64db9af28b11d945c4f18aff7020c80c6656009';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  increaseStaking() {
+
+  }
+
+  decreaseStaking() {
+    
   }
 
 }
