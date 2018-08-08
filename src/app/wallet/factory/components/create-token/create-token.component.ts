3587d177cc12d667dd1b4bfe933094b0203492a6
Petro Sidlovsky <sidlovskyy@gmail.com>
2018-08-09 00:55:33 +0300

    Form validation added

--- src/app/wallet/factory/components/create-token/create-token.component.ts
+++ src/app/wallet/factory/components/create-token/create-token.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-create-token',
@@ -7,18 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTokenComponent implements OnInit {
 
+  locked = false;
+
   name: string;
   symbol: string;
   supply: number;
   decimals: number;
   address: string;
 
-  constructor() { }
+  createForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.maxLength(100), Validators.pattern("^[0-9A-Za-z- ]+$")]],
+      symbol: [null, [Validators.required, Validators.maxLength(10), Validators.pattern("^[0-9A-Z]+$")]],
+      supply: [null, [Validators.required, Validators.pattern("^[0-9]{1,10}$"),  Validators.min(1)]],
+      decimals: [null, [Validators.required, Validators.pattern("^[0-9]+$"), Validators.min(0), Validators.max(18)]]
+    });
   }
 
   async createToken() {
     this.address = "abc";
+  }
+
+  canCreateToken() {
+    return !this.locked && this.createForm.valid;
+  }
+
+  hasError(control: AbstractControl): boolean {
+    if(!control) {
+      return false;
+    }
+
+    return !control.valid && control.touched;
   }
 }
