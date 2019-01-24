cb91d5a73f92ff9e2121bcf082b51671ce16504b
Volodymyr Matskiv <volodymyr.matskiv@devrecords.com>
2019-01-24 14:11:34 +0200

    Fixed scroll issues for mobile devices

--- src/app/wallet/swap/components/cross-chain-swap-list/cross-chain-swap-list.component.ts
+++ src/app/wallet/swap/components/cross-chain-swap-list/cross-chain-swap-list.component.ts
@@ -8,7 +8,7 @@ import { AuthenticationService } from "@core/authentication/authentication-servi
 import { InternalNotificationService } from "@core/general/internal-notification-service/internal-notification.service";
 import { LoggerService } from "@core/general/logger-service/logger.service";
 import { SwapType } from '@app/core/swap/models/swap-type.enum';
-import { isAndroidDevice } from '@app/shared/helpers/platform-utils';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-cross-chain-swap-list',
@@ -25,7 +25,7 @@ export class CrossChainSwapListComponent implements OnInit {
   canShowMore = true;
   swaps: SwapListItem[] = [];
   allSwaps: SwapListItem[] = [];
-  perfectScrollbarDisabled = isAndroidDevice();
+  perfectScrollbarDisabled = environment.isMobileBuild;
 
   constructor(
     private logger: LoggerService,
