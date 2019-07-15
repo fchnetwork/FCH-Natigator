7446f0b2d921289fc797b7fea877e77d51b3bb93
Volodymyr Matskiv <volodymyr.matskiv@devrecords.com>
2019-07-15 14:45:44 +0300

    Fixed AERW-624-clarify-a-need-of-choose-a-deri

--- src/app/wallet/settings/components/derivation-path/derivation-path.component.ts
+++ src/app/wallet/settings/components/derivation-path/derivation-path.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { NotificationMessagesService } from '@core/general/notification-messages-service/notification-messages.service';
 import { SettingsService } from '@app/core/settings/settings.service';
-
-export interface iDerivationPaths {
-  id: number;
-  derivation:string;
-  disabled: boolean;
-}
 
 @Component({
   selector: 'app-derivation-path',
@@ -14,42 +7,10 @@ export interface iDerivationPaths {
   styleUrls: ['./derivation-path.component.scss']
 })
 export class DerivationPathComponent {
-
-  selectResult: any;
   activeDerivation: string;
-  
-  derivationPaths: Array<iDerivationPaths> = [{
-    id: 1,
-    derivation: "m/44'/60'/0'/0/0",
-    disabled: false,
-  },{
-    id: 2,
-    derivation: "m/44'/312'/0'/0/0",
-    disabled: true,
-  }]  
-  
-  constructor(
-      private notificationService: NotificationMessagesService,
-      private settingsService: SettingsService ) 
-  {
-    let activeDerivation = this.settingsService.settings.generalSettings.derivationPath;
-                                 
-    this.derivationPaths.forEach( (path, i) => {
-      if (path.derivation == activeDerivation ) {
-        this.selectResult = this.derivationPaths[i];
-      }
-    });  
-  }
- 
-  /**
-   * Save derivation path to cookie when selected from dropdown
-   *
-   * @param {*} evt
-   * @memberof DerivationPathComponent
-   */
-  derivationChanged(evt: iDerivationPaths){
-    this.settingsService.saveSetting("generalSettings", "derivationPath", evt.derivation)
-    this.notificationService.derivationModified(evt.derivation);
-  }
 
+  constructor(private settingsService: SettingsService) {
+    const settings = this.settingsService.getSettings();
+    this.activeDerivation = settings.generalSettings.derivationPath;
+  }
 }
